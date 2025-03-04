b028bb7b917e94f7ee0a0683e68588bc1bd7ed04
ellaiv <iovumihaela4@gmail.com>
2025-03-04 17:33:29 +0200

    Start a new management project

--- src/App.jsx
+++ src/App.jsx
@@ -123,6 +123,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
